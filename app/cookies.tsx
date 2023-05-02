f815e659e32aec8dfaf976fea091b31d7b176781
Nathan Rajlich <n@n8.io>
2023-05-02 02:35:14 -0700

    Remove unused code

--- app/cookies.tsx
+++
@@ -1,7 +0,0 @@
-import { createCookie } from '@vercel/remix';
-
-export const sessionCookie = createCookie('__session', {
-	sameSite: 'strict',
-	secrets: ['s3cret1-nsp'],
-	httpOnly: true,
-});
--- app/session.server.ts
+++
@@ -1,9 +0,0 @@
-import { createCookieSessionStorage } from '@vercel/remix';
-import { sessionCookie } from './cookies';
-
-const { getSession, commitSession, destroySession } =
-	createCookieSessionStorage({
-		cookie: sessionCookie,
-	});
-
-export { getSession, commitSession, destroySession };
