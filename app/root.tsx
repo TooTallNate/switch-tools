a1dd6135d94a20edef983e542f66adc9b8e7eb1d
Nathan Rajlich <n@n8.io>
2023-05-07 00:56:45 -0700

    Add "description" meta tag

--- app/root.tsx
+++ app/root.tsx
@@ -27,6 +27,7 @@ export const config = { runtime: 'edge' };
 export const meta: MetaFunction = () => ({
 	charset: 'utf-8',
 	title: 'NSP Forwarder Generator',
+	description: 'Create "NRO to NSP forwarders" for your modded Nintendo Switch.',
 	viewport: 'width=device-width,initial-scale=1',
 });
 
